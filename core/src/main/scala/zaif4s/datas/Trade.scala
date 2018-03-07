4b90ec0ac29f68c879dfd5b18e8fb2edac3cd055
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-03-07 15:30:40 +0900

    Clean: apply method

--- core/src/main/scala/zaif4s/datas/Trade.scala
+++ core/src/main/scala/zaif4s/datas/Trade.scala
@@ -9,24 +9,20 @@ case class Trade(
   tradeType: TradeType
 )
 
-sealed trait TradeType {
-  def name: String
-}
+sealed abstract class TradeType(val name: String)
 
-case object BID extends TradeType {
-  override def name: String = "bid"
+case object BID extends TradeType("bid") {
   override def toString: String = name
 }
 
-case object ASK extends TradeType {
-  override def name: String = "ask"
+case object ASK extends TradeType("ask") {
   override def toString: String = name
 }
 
 object TradeType {
   def apply(name: String): TradeType = name match {
-    case "bid" => BID
-    case "ask" => ASK
+    case BID.name => BID
+    case ASK.name => ASK
     case _ => throw new RuntimeException(s"Invalid TradeType. Input: $name")
   }
 }
